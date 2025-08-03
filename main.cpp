07fbf215afd592e7e5326be3f1127cd253fcf55b
Igur7 <115376029+Igur7@users.noreply.github.com>
2025-08-03 13:38:19 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "SudokuBoard.hpp"
+#include <iostream>
+
+int main() {
+    SudokuBoard board;
+    board.generatePuzzle();
+
+    while (true) {
+        board.display();
+        int row, col, num;
+        std::cout << "Wprowadź wiersz (0-8), kolumnę (0-8) i liczbę (1-9), lub -1 by wyjść: ";
+        std::cin >> row;
+        if (row == -1) break;
+        std::cin >> col >> num;
+
+        if (row < 0 || row > 8 || col < 0 || col > 8 || num < 1 || num > 9) {
+            std::cout << "Nieprawidłowe dane.\n";
+            continue;
+        }
+
+        if (board.isValidMove(row, col, num)) {
+            board.setCell(row, col, num);
+        } else {
+            std::cout << "Nieprawidłowy ruch!\n";
+        }
+    }
+
+    std::cout << "Koniec gry.\n";
+    return 0;
+}
