17c3ad66a6a8023d3fdde6aedead89ffb507fbbe
Igur7 <115376029+Igur7@users.noreply.github.com>
2025-08-03 13:37:27 +0200

    Update SudokuBoard.hpp

--- SudokuBoard.hpp
+++ SudokuBoard.hpp
@@ -1,1 +1,18 @@
+#ifndef SUDOKUBOARD_HPP
+#define SUDOKUBOARD_HPP
+#include <array>
 
+class SudokuBoard {
+private:
+  std::array<std::array<int, 9>, 9> board;
+
+public:
+  SudokuBoard();
+  void generatePuzzle();
+  void display() const;
+  bool isValidMove(int row, int col, int num) const;
+  void setCell(int row, int col, int num);
+  int getCell(int row, int col) const;
+};
+
+#endif //SUDOKUBOARD_HPP
